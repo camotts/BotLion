97be60ccb0ef3c4e8b2ce04f936f3f47e793ed57
camotts <otts.cameron@gmail.com>
2018-12-10 11:00:23 -0600

    added brackets to suppress url preview

--- middleware/github.go
+++ middleware/github.go
@@ -19,7 +19,7 @@ func NewGithubMiddleware(s *discordgo.Session, next MessageMiddleware, githubURL
 
 func (g GithubMiddleware) Handle(m *discordgo.MessageCreate) MessageMiddleware {
 	if strings.HasPrefix(m.Content, "++github") {
-		g.s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("You can find me at %s", g.githubURL))
+		g.s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("You can find me at <%s>", g.githubURL))
 		return nil
 	}
 	return g.next.Handle(m)
