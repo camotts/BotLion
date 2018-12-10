5bffa50511947cab58eb84b52b72e7eb9116a08a
Nick Dolan <nicholas.dolan1@gmail.com>
2018-12-10 16:28:22 -0600

    feat(google-it): Added empty check

--- middleware/google_it.go
+++ middleware/google_it.go
@@ -20,6 +20,10 @@ func NewGoogleItMiddleware(s *discordgo.Session, next MessageMiddleware) Message
 
 func (p googleItMiddleware) Handle(m *discordgo.MessageCreate) MessageMiddleware {
 	if strings.HasPrefix(m.Content, "++google-it") {
+		if strings.TrimSpace(m.Content) == "++google-it" {
+			p.s.ChannelMessageSend(m.ChannelID, "You should be able to figure it out from here. <https://google.com>")
+			return nil
+		}
 		p.s.ChannelMessageSend(m.ChannelID,
 			"Here's a super helpful link! <https://lmgtfy.com/?q="+
 				url.QueryEscape(strings.TrimSpace(strings.TrimPrefix(m.Content, "++google-it"))) +
